a89dff6fb60ddee44fbfef107714488b0b197ddb
Belal Hamdy <36121094+belalhamdy@users.noreply.github.com>
2019-11-27 13:57:21 +0200

    initialize

--- src/com/company/ArithmeticCoding.java
+++ src/com/company/ArithmeticCoding.java
@@ -4,22 +4,37 @@ import java.math.BigDecimal;
 import java.nio.file.Path;
 
 public class ArithmeticCoding {
+    static final int MAX_CHARACTERS = 256;
     static BigDecimal lower,upper,range,value;
-    static void init() {
+    static BigDecimal[] cumulative;
+    static void init(double[] prob) {
         lower = new BigDecimal(0);
         upper = new BigDecimal(0);
         range = new BigDecimal(0);
+        cumulative = new BigDecimal[MAX_CHARACTERS];
+        assert prob.length == MAX_CHARACTERS;
+
+
     }
 
     public static String Encode(String data,double[] prob) {
-        init();
+        init(prob);
 
         return null;
     }
 
     public static String Decode(String data,double[] prob) {
-        init();
+        init(prob);
         return null;
+    }
+    static BigDecimal getLowRange(char s){
+        return null;
+    }
+    static BigDecimal getHighRange(char s){
+        return null;
+    }
+    static char findByValue(BigDecimal val){
+        return '\0';
     }
     static String DecimalToFloat (BigDecimal val)
     {
